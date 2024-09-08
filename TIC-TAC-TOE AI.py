d81e2c8ca5c9f79aeca28e9278ead8277f8cac75
SHIVAM ARORA <shivam.pvt.in@gmail.com>
2024-09-08 19:48:17 +0530

    Add files via upload

---
+++ TIC-TAC-TOE AI.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import math
+
+# Initialize the board
+board = [' ' for _ in range(9)]
+
+def print_board():
+    for row in [board[i*3:(i+1)*3] for i in range(3)]:
+        print('| ' + ' | '.join(row) + ' |')
+
+def check_winner(board, player):
+    win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
+    return any(board[a] == board[b] == board[c] == player for a, b, c in win_conditions)
+
+def minimax(board, depth, is_maximizing):
+    if check_winner(board, 'O'):
+        return 1
+    elif check_winner(board, 'X'):
+        return -1
+    elif ' ' not in board:
+        return 0
+
+    if is_maximizing:
+        best_score = -math.inf
+        for i in range(9):
+            if board[i] == ' ':
+                board[i] = 'O'
+                score = minimax(board, depth + 1, False)
+                board[i] = ' '
+                best_score = max(score, best_score)
+        return best_score
+    else:
+        best_score = math.inf
+        for i in range(9):
+            if board[i] == ' ':
+                board[i] = 'X'
+                score = minimax(board, depth + 1, True)
+                board[i] = ' '
+                best_score = min(score, best_score)
+        return best_score
+
+def best_move():
+    best_score = -math.inf
+    move = 0
+    for i in range(9):
+        if board[i] == ' ':
+            board[i] = 'O'
+            score = minimax(board, 0, False)
+            board[i] = ' '
+            if score > best_score:
+                best_score = score
+                move = i
+    return move
+
+def play_game():
+    print_board()
+    while ' ' in board:
+        # Human move
+        move = int(input("Enter your move (1-9): ")) - 1
+        if board[move] != ' ':
+            print("Invalid move! Try again.")
+            continue
+        board[move] = 'X'
+        if check_winner(board, 'X'):
+            print_board()
+            print("You win!")
+            return
+        if ' ' not in board:
+            print_board()
+            print("It's a tie!")
+            return
+
+        # AI move
+        board[best_move()] = 'O'
+        print_board()
+        if check_winner(board, 'O'):
+            print("AI wins!")
+            return
+        if ' ' not in board:
+            print("It's a tie!")
+            return
+
+play_game()
+
+
+# In[ ]:
+
+
+
+
